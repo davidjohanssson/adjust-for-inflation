bc219d2edd4e78a52e0e35103d59e7ab56534fc7
David Johansson <david.n.johansson@live.com>
2024-04-01 15:30:34 +0200

    ..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,51 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepicker, MatDatepickerModule } from '@angular/material/datepicker';
+import * as _moment from 'moment';
+import { default as _rollupMoment, Moment } from 'moment';
+import moment from 'moment';
+import sv from '@angular/common/locales/sv';
+import { registerLocaleData } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     MatButtonModule,
-    MatIconModule
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    ReactiveFormsModule
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public formGroup = new FormGroup({
+    amount: new FormControl<number | null>(null, { validators: [Validators.required, Validators.min(0)] }),
+    from: new FormControl(),
+    // through: new FormControl()
+  });
 
+  constructor() {
+    moment.locale('sv');
+    registerLocaleData(sv);
+  }
+
+  public setMonthAndYear(normalizedMonthAndYear: Moment, datepicker: MatDatepicker<Moment>) {
+    const ctrlValue = this.formGroup.controls.from.value ?? moment();
+    ctrlValue.month(normalizedMonthAndYear.month());
+    ctrlValue.year(normalizedMonthAndYear.year());
+    this.formGroup.controls.from.setValue(ctrlValue);
+    datepicker.close();
+  }
+
+  public handleSubmit() {
+
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,48 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MAT_MOMENT_DATE_ADAPTER_OPTIONS, MomentDateAdapter } from '@angular/material-moment-adapter';
+import { MAT_ICON_DEFAULT_OPTIONS } from '@angular/material/icon';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import 'moment/locale/sv';
+
+const CUSTOM_DATE_FORMATS = {
+  parse: {
+    dateInput: 'MMMM YYYY',
+  },
+  display: {
+    dateInput: 'MMMM YYYY',
+    monthYearLabel: 'MMMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  }
+};
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideAnimationsAsync(),
+    provideRouter(routes),
+    {
+      provide: LOCALE_ID,
+      useValue: 'sv-SE'
+    },
+    {
+      provide: MAT_ICON_DEFAULT_OPTIONS,
+      useValue: { fontSet: 'material-icons-outlined' }
+    },
+    {
+      provide: MAT_DATE_FORMATS,
+      useValue: CUSTOM_DATE_FORMATS
+    },
+    {
+      provide: MAT_DATE_LOCALE,
+      useValue: 'sv-SE'
+    },
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS, MAT_DATE_FORMATS]
+    }
+  ]
 };
